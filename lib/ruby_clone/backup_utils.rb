1086e05e4054fe0e9d7a83d3fdba50a85ea71f48
Frederico Benevides <fredbene@gmail.com>
2012-09-23 20:13:00 -0300

    Refactoring and fixed how BackupUtils should delete old files with same name

--- lib/ruby_clone/backup_utils.rb
+++ lib/ruby_clone/backup_utils.rb
@@ -6,31 +6,33 @@ module RubyClone
   module BackupUtils
 
     def self.delete_files(path, ruby_clone_suffix, limit)
-      finder = @find || Find
 
-      find_files_with_same_name finder, path, ruby_clone_suffix
-      delete_files_reached_the_limit limit
-    end
-
-   private
-
-    def self.find_files_with_same_name(finder, path, ruby_clone_suffix)
-      @paths = {}
-
-      finder.find(path) do |path|
-        path_without_suffix = path.sub(/#{ruby_clone_suffix}.*/, '')
-
-        @paths[path_without_suffix] ||= []
-        @paths[path_without_suffix] << path
+      if File.exists?(path)
+        paths = find_files_with_same_name path, ruby_clone_suffix
+        delete_files_reached_the_limit paths, limit
       end
     end
 
-    def self.delete_files_reached_the_limit(limit)
-      file_utils = @file_utils || FileUtils
-      @paths.each_value do |files|
+    def self.find_files_with_same_name(path, ruby_clone_suffix)
+      paths = {}
+
+      Find.find(path) do |path|
+        path_without_suffix = path.sub(/#{ruby_clone_suffix}.*/, '')
+
+        if FileTest.file? path
+          paths[path_without_suffix] ||= []
+          paths[path_without_suffix] << path
+        end
+      end
+
+      paths
+    end
+
+    def self.delete_files_reached_the_limit(paths, limit)
+      paths.each_value do |files|
         if files.size > limit
-          files.slice(limit, files.size).each do |file|
-            file_utils.remove_entry file
+          files.slice(0, files.size - limit).each do |file|
+            FileUtils.remove_entry file
           end
         end
       end
--- spec/ruby_clone/backup_utils_spec.rb
+++ spec/ruby_clone/backup_utils_spec.rb
@@ -9,39 +9,60 @@ module RubyClone
       before(:each) do
         @ruby_clone_suffix = '_rbcl'
 
-        @paths = [ "/my_path/sub_path1/file1#{@ruby_clone_suffix}_yesterday",
-                   "/my_path/sub_path1/file1#{@ruby_clone_suffix}_today",
-                   "/my_path/sub_path1/file2#{@ruby_clone_suffix}_yesterday",
-                   "/my_path/sub_path2/file1#{@ruby_clone_suffix}_yesterday",
-                   "/my_path/sub_path2/file2_rbcl_today"]
+        @paths = [ "/my_path/sub_path1/file1#{@ruby_clone_suffix}_20120921",
+                   "/my_path/sub_path1/file1#{@ruby_clone_suffix}_20120922",
+                   "/my_path/sub_path1/file1#{@ruby_clone_suffix}_20120923",
 
-        BackupUtils.instance_eval { @find = Find }
-        BackupUtils.instance_eval { @file_utils = FileUtils }
+                   "/my_path/sub_path1/file2#{@ruby_clone_suffix}_20120922",
+                   "/my_path/sub_path2/file1#{@ruby_clone_suffix}_20120922",
+                   "/my_path/sub_path2/file2#{@ruby_clone_suffix}_20120923"]
 
-        Find.stub(:find).and_yield(@paths[0]).and_yield(@paths[1]).and_yield(@paths[2])
-                        .and_yield(@paths[3]).and_yield(@paths[4])
+        File.stub(:exists?).with(any_args()).and_return(true)
+
+        FileTest.stub(:file?).with(any_args()).and_return false
+        FileTest.stub(:file?).with(@paths[0]).and_return true
+        FileTest.stub(:file?).with(@paths[1]).and_return true
+        FileTest.stub(:file?).with(@paths[2]).and_return true
+        FileTest.stub(:file?).with(@paths[3]).and_return true
+        FileTest.stub(:file?).with(@paths[4]).and_return true
+        FileTest.stub(:file?).with(@paths[5]).and_return true
+
+        Find.stub(:find).and_yield('/my_path').and_yield('/my_path/sub_path1')
+                        .and_yield(@paths[0]).and_yield(@paths[1]).and_yield(@paths[2]).and_yield(@paths[3])
+                        .and_yield('/my_path/sub_path2')
+                        .and_yield(@paths[4]).and_yield(@paths[5])
 
       end
 
       describe "valid" do
 
         it "should create a hash with [path_files_without_suffix, path_files] that points to the files with same name" do
-          BackupUtils.delete_files('/my_path', @ruby_clone_suffix, 5)
+          paths = BackupUtils.find_files_with_same_name '/my_path', @ruby_clone_suffix
 
-          BackupUtils.instance_eval { @paths }.should == { '/my_path/sub_path1/file1' => [@paths[0], @paths[1]],
-                                                           '/my_path/sub_path1/file2' => [@paths[2]],
-                                                           '/my_path/sub_path2/file1' => [@paths[3]],
-                                                           '/my_path/sub_path2/file2' => [@paths[4]] }
+          paths.should == { '/my_path/sub_path1/file1' => [@paths[0], @paths[1], @paths[2]],
+                            '/my_path/sub_path1/file2' => [@paths[3]],
+                            '/my_path/sub_path2/file1' => [@paths[4]],
+                            '/my_path/sub_path2/file2' => [@paths[5]] }
         end
 
         it "should delete old files with same name but different 'suffix' when passed the limit" do
-          FileUtils.should_receive(:remove_entry).once
+          FileUtils.should_receive(:remove_entry).with(@paths[0])
+          FileUtils.should_receive(:remove_entry).with(@paths[1])
+          FileUtils.should_not_receive(:remove_entry).with(@paths[2])
+
           BackupUtils.delete_files('/my_path', @ruby_clone_suffix, 1)
         end
 
       end
 
       describe "invalid" do
+
+        it "should not delete files with path that doesn't exist" do
+          File.stub(:exists?).with(any_args()).and_return(false)
+          Find.should_not_receive(:find)
+
+          BackupUtils.delete_files('/my_path', @ruby_clone_suffix, 5)
+        end
 
         it "should not delete files with same name but different 'suffix' when it didn't pass the limit" do
           FileUtils.should_not_receive(:remove_entry)
@@ -50,7 +71,7 @@ module RubyClone
 
         it "should not delete files with same name but different 'suffix' when the found files is on the limit" do
           FileUtils.should_not_receive(:remove_entry)
-          BackupUtils.delete_files('/my_path', @ruby_clone_suffix, 2)
+          BackupUtils.delete_files('/my_path', @ruby_clone_suffix, 3)
         end
 
       end
